c9a220505208d89a62b732873f9e3c9005437b9e
Kathe <Vasquezdianakatherine@gmail.com>
2024-06-20 17:04:43 -0500

    correcciones

---
+++ exposiciones/ejercicios.js
@@ -0,0 +1,66 @@
+/* 1 : array length */
+/* frutas = ["Naranja","Manzana","Pera","Banano"];
+console.log(frutas length);
+
+const frutas = "Naranja";
+console.log(cadena.length);
+ */
+
+/* 2 : array to string */
+/* let fruta = ["Naranja","Manzana","Pera","Banano"];
+console.log(frutas.toString());
+
+
+let vehiculos =["camioneta","taxi","bus","moto"];
+console.log
+ */
+
+
+/* 3 : array atob() */
+/* const frut = ["Mango","Sandia","Manzana","Pera"];
+let fruti = frut [3]
+console.log(fruti);
+
+
+const fru = ["Mango","Sandia","Manzana","Pera"];
+let fruta = frutas.flat(-1)
+console.log(fruta);
+ */
+
+
+/* 4 : Array join() */
+/* const fruits = ["Banano","Naranja","Manzana","Mango"];
+console.log(fruits.join("*"));
+
+const num = [1,2,3,4];
+console.log(num.join("."));
+ */
+
+
+
+/* 5 : Array copyWitnin */
+/* let fruits = ["Banana","orange","apple","Mango"];
+fruits copywithin(2,0);
+
+let num = [1,2,3,4,5,6]
+console.log(num.copyWithin(1,0));
+ */
+
+
+
+/* 6 : Array push() */
+/* let contar = frutas push ('fresa');
+console.log(contar);
+
+console.log(frutas);
+ */
+
+
+
+/* 7 : Array unshift() */
+/* let frutas = ["manzana","Pera","uva"];
+console.log(frutas.unshift('banano'));
+
+
+const notas = [10,20,30];
+console.log(notas.unshift(40,50)); */
